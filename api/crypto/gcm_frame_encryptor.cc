d6518c571f9a13ee90d0e24481e1a29101dcd0c2
titus.moldovan <titus.moldovan@8x8.com>
2021-05-25 12:09:31 +0300

    another fix

--- api/crypto/gcm_frame_encryptor.cc
+++ api/crypto/gcm_frame_encryptor.cc
@@ -111,7 +111,9 @@ int GCMFrameEncryptor::Encrypt(cricket::MediaType media_type,
   
   unsigned char *outbuf = aes_gcm_encrypt(frame);
 
+    RTC_LOG(LS_VERBOSE) << "XXX aes_gcm_encrypt4";
   for (uint8_t i = 0; i < sizeof(outbuf); i++) {
+      RTC_LOG(LS_VERBOSE) << "XXX aes_gcm_encrypt41";
        encrypted_frame[unencrypted_bytes + i] = frame[i];
   }
 
