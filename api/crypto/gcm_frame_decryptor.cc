501761c5a8c8e73421b1420aa129db9696d05e45
titus.moldovan <titus.moldovan@8x8.com>
2021-06-18 16:28:49 +0300

    stuff

--- api/crypto/gcm_frame_decryptor.cc
+++ api/crypto/gcm_frame_decryptor.cc
@@ -111,7 +111,8 @@ GCMFrameDecryptor::Result GCMFrameDecryptor::Decrypt(
 
   // Frame trailer
   size_t frame_trailer_size = 2;
-  std::vector<uint8_t> frame_trailer = new std::vector<uint8_t> (frame_trailer_size);
+  std::vector<uint8_t> frame_trailer;
+  frame_trailer.reserve(frame_trailer_size);
   frame_trailer[0] = encrypted_frame[encrypted_frame.size() - 2]; //IV_LENGHT
   frame_trailer[1] = encrypted_frame[encrypted_frame.size() - 1]; 
   
