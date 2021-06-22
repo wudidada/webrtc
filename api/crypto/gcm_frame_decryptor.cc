79c9ec185557648d1b0ee887bb5e41fb468713c1
titus.moldovan <titus.moldovan@8x8.com>
2021-06-22 15:24:45 +0300

    revert this

--- api/crypto/gcm_frame_decryptor.cc
+++ api/crypto/gcm_frame_decryptor.cc
@@ -92,6 +92,98 @@ std::vector<uint8_t> aes_gcm_decrypt(std::vector<uint8_t> encrypted_frame,
     return outbuf;
 }
 
+int new_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int plaintext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        RTC_LOG(LS_VERBOSE) << "XXX decrypting error 21------------------------";
+
+    /*
+     * Initialise the decryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+         RTC_LOG(LS_VERBOSE) << "XXX decrypting error 22------------------------";
+
+    /*
+     * Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary.
+     */
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+         RTC_LOG(LS_VERBOSE) << "XXX decrypting error 23------------------------";
+    plaintext_len = len;
+
+    /*
+     * Finalise the decryption. Further plaintext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        RTC_LOG(LS_VERBOSE) << "XXX decrypting error 24------------------------";
+    plaintext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    RTC_LOG(LS_VERBOSE) << "XXX decrht plaintext_len------------------------" << plaintext_len;
+    return plaintext_len;
+}
+
+int new_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int ciphertext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        RTC_LOG(LS_VERBOSE) << "XXX encrypting error 21------------------------";
+
+    /*
+     * Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+         RTC_LOG(LS_VERBOSE) << "XXX encrypting error 22------------------------";
+
+    /*
+     * Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+         RTC_LOG(LS_VERBOSE) << "XXX encrypting error 23------------------------";
+    ciphertext_len = len;
+
+    /*
+     * Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+         RTC_LOG(LS_VERBOSE) << "XXX encrypting error 24------------------------";
+    ciphertext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    RTC_LOG(LS_VERBOSE) << "XXX encrypting ciphertext_len------------------------" << ciphertext_len;
+    return ciphertext_len;
+}
+
 GCMFrameDecryptor::Result GCMFrameDecryptor::Decrypt(
     cricket::MediaType media_type,
     const std::vector<uint32_t>& csrcs,
@@ -164,6 +256,45 @@ GCMFrameDecryptor::Result GCMFrameDecryptor::Decrypt(
  // RTC_LOG(LS_VERBOSE) << "XXX decrypting5------------------------" << additional_data.size();
   RTC_LOG(LS_VERBOSE) << "XXX decrypting6------------------------" << payload_lenght;
  // RTC_LOG(LS_VERBOSE) << "XXX decrypting71------------------------" << iv;
+
+
+/*
+     * Set up the key and iv. Do I need to say to not hard code these in a
+     * real application? :-)
+     */
+
+    /* A 256 bit key */
+    unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+
+    /* A 128 bit IV */
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+
+    /* Message to be encrypted */
+    unsigned char *plaintext =
+        (unsigned char *)"The quick brown fox jumps over the lazy dog";
+
+    /*
+     * Buffer for ciphertext. Ensure the buffer is long enough for the
+     * ciphertext which may be longer than the plaintext, depending on the
+     * algorithm and mode.
+     */
+    unsigned char ciphertext[128];
+
+    /* Buffer for the decrypted text */
+    unsigned char decryptedtext[128];
+
+    int decryptedtext_len, ciphertext_len;
+
+    /* Encrypt the plaintext */
+    ciphertext_len = new_encrypt (plaintext, strlen ((char *)plaintext), key, iv,
+                              ciphertext);
+
+    /* Decrypt the ciphertext */
+    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
+                                decryptedtext);
+
+    /* Add a NULL terminator. We are expecting printable text */
+    decryptedtext[decryptedtext_len] = '\0';
 
   return Result(Status::kOk, frame.size());
 }
