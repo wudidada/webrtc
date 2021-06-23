b4776257d2624fa02af734a67a319d3f0e61b181
titus.moldovan <titus.moldovan@8x8.com>
2021-06-23 11:30:22 +0300

    better?

--- api/crypto/gcm_frame_decryptor.cc
+++ api/crypto/gcm_frame_decryptor.cc
@@ -288,8 +288,14 @@ GCMFrameDecryptor::Result GCMFrameDecryptor::Decrypt(
 
     RTC_LOG(LS_VERBOSE) << "XXX newEncrypt1------------------------";*/
 
+    unsigned char gcm_key1[] = {
+    195, 130, 222, 164, 47, 57, 241, 245, 151, 138, 25, 165, 95, 71, 146, 
+                 67, 189, 29, 194, 5, 9, 22, 33, 224, 139, 35, 60, 122, 146, 97, 169, 206
+    };
+
+
     /* Decrypt the ciphertext */
-    decryptedtext_len = new_decrypt(&payload[0], payload_lenght, gcm_key, &iv[0],
+    decryptedtext_len = new_decrypt(&payload[0], payload_lenght, gcm_key1, &iv[0],
                                 decryptedtext);
     RTC_LOG(LS_VERBOSE) << "XXX newEncrypt2------------------------" << decryptedtext;
 
