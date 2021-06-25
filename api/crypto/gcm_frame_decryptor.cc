83cb88dedbf3d37278ad527d7c5937ab1b2660ac
titus.moldovan <titus.moldovan@8x8.com>
2021-06-25 15:00:21 +0300

    chages back key to char

--- api/crypto/gcm_frame_decryptor.cc
+++ api/crypto/gcm_frame_decryptor.cc
@@ -316,7 +316,7 @@ GCMFrameDecryptor::Result GCMFrameDecryptor::Decrypt(
 
     int decryptedtext_len, ciphertext_len;
 
-    uint8_t gcm_key1[] = {
+    unsigned char gcm_key1[] = {
                  195, 130, 222, 164, 47, 57, 241, 245, 151, 138, 25, 165, 95, 71, 146, 
                  67, 189, 29, 194, 5, 9, 22, 33, 224, 139, 35, 60, 122, 146, 97, 169, 206
     };
