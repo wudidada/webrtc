eb2b884f5ad007c5bfc52c59c0904711252f24d3
titus.moldovan <titus.moldovan@8x8.com>
2021-07-05 12:41:21 +0300

    caaaaareful here, adds cpp encryption

--- api/crypto/gcm_frame_decryptor.cc
+++ api/crypto/gcm_frame_decryptor.cc
@@ -6,7 +6,83 @@
 #include <vector>
 #include "rtc_base/logging.h"
 
+#include <iostream>
+#include <string>
+#include <memory>
+#include <limits>
+#include <stdexcept>
+
+#include <openssl/rand.h>
+
 namespace webrtc {
+
+static const unsigned int KEY_SIZE = 32;
+static const unsigned int BLOCK_SIZE = 16;
+
+template <typename T>
+struct zallocator
+{
+public:
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+
+    pointer address (reference v) const {return &v;}
+    const_pointer address (const_reference v) const {return &v;}
+
+    pointer allocate (size_type n, const void* hint = 0) {
+        if (n > std::numeric_limits<size_type>::max() / sizeof(T))
+            RTC_LOG(LS_VERBOSE) << "XXX bad alloc";
+        return static_cast<pointer> (::operator new (n * sizeof (value_type)));
+    }
+
+    void deallocate(pointer p, size_type n) {
+        OPENSSL_cleanse(p, n*sizeof(T));
+        ::operator delete(p); 
+    }
+    
+    size_type max_size() const {
+        return std::numeric_limits<size_type>::max() / sizeof (T);
+    }
+    
+    template<typename U>
+    struct rebind
+    {
+        typedef zallocator<U> other;
+    };
+
+    void construct (pointer ptr, const T& val) {
+        new (static_cast<T*>(ptr) ) T (val);
+    }
+
+    void destroy(pointer ptr) {
+        static_cast<T*>(ptr)->~T();
+    }
+
+#if __cpluplus >= 201103L
+    template<typename U, typename... Args>
+    void construct (U* ptr, Args&&  ... args) {
+        ::new (static_cast<void*> (ptr) ) U (std::forward<Args> (args)...);
+    }
+
+    template<typename U>
+    void destroy(U* ptr) {
+        ptr->~U();
+    }
+#endif
+};
+
+typedef unsigned char byte;
+typedef std::basic_string<char, std::char_traits<char>, zallocator<char> > secure_string;
+using EVP_CIPHER_CTX_free_ptr = std::unique_ptr<EVP_CIPHER_CTX, decltype(&::EVP_CIPHER_CTX_free)>;
+
+void gen_params(byte key[KEY_SIZE], byte iv[BLOCK_SIZE]);
+void aes_encrypt(const byte key[KEY_SIZE], const byte iv[BLOCK_SIZE], const secure_string& ptext, secure_string& ctext);
+void aes_decrypt(const byte key[KEY_SIZE], const byte iv[BLOCK_SIZE], const secure_string& ctext, secure_string& rtext);
 
 static const unsigned char gcm_key[] = {
     195, 130, 222, 164, 47, 57, 241, 245, 151, 138, 25, 165, 95, 71, 146, 
@@ -68,7 +144,7 @@ int new_decrypt(unsigned char *ciphertext,
 
     int plaintext_len;
 
-    for (size_t i =0 ; i < ciphertext_len; i++) {
+    for (size_t i = 0 ; i < ciphertext_len; i++) {
       RTC_LOG(LS_VERBOSE) << "XXX decrypting initial------------------------" << myUniqueId<< " " << i << " " << ciphertext[i];
     }
 
@@ -93,8 +169,8 @@ int new_decrypt(unsigned char *ciphertext,
      * Provide any AAD data. This can be called zero or more times as
      * required
      */
-    if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len))
-         RTC_LOG(LS_VERBOSE) << "XXX decrypting error 222------------------------";
+   // if(!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len))
+   //      RTC_LOG(LS_VERBOSE) << "XXX decrypting error 222------------------------";
     /*
      * Provide the message to be decrypted, and obtain the plaintext output.
      * EVP_DecryptUpdate can be called multiple times if necessary.
@@ -103,9 +179,8 @@ int new_decrypt(unsigned char *ciphertext,
          RTC_LOG(LS_VERBOSE) << "XXX decrypting error 23------------------------";
     plaintext_len = len;
 
-    if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag)) {
-        RTC_LOG(LS_VERBOSE) << "XXX decrypting error 231------------------------";
-    }
+    //if(!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag)) {
+      //  RTC_LOG(LS_VERBOSE) << "XXX decrypting error 231------------------------";
     /*
      * Finalise the decryption. Further plaintext bytes may be written at
      * this stage.
@@ -197,18 +272,24 @@ GCMFrameDecryptor::Result GCMFrameDecryptor::Decrypt(
 
     RTC_LOG(LS_VERBOSE) << "XXX newEncrypt------------------------";
 
-    unsigned char *ciphertext =
-        (unsigned char *)"165, 160, 86, 14, 124, 65, 187, 52, 78, 152, 82, 216, 18, 133, 240, 185, 88, 174, 180";
+    // Load the necessary cipher
+    EVP_add_cipher(EVP_aes_256_cbc());
 
-    new_decrypt(
-            ciphertext, 
-            sizeof(ciphertext), 
-            gcm_key1, 
-            &frame_header[0],
-            frame_header_size,
-            &iv1[0], 
-            decryptedtext, 
-            tag);
+    // plaintext, ciphertext, recovered text
+    secure_string ptext = "Now is the time for all good men to come to the aide of their country";
+    secure_string ctext, rtext;
+
+    byte key[KEY_SIZE], iv[BLOCK_SIZE];
+    gen_params(key, iv);
+  
+    aes_encrypt(key, iv, ptext, ctext);
+    aes_decrypt(key, iv, ctext, rtext);
+    
+    OPENSSL_cleanse(key, KEY_SIZE);
+    OPENSSL_cleanse(iv, BLOCK_SIZE);
+
+    RTC_LOG(LS_VERBOSE) << "XXX Original message: " << ptext;
+    RTC_LOG(LS_VERBOSE) << "XXX Recovered message" << rtext;
 
     /* Decrypt the ciphertext */
    /* decryptedtext_len = new_decrypt(
@@ -235,6 +316,65 @@ size_t GCMFrameDecryptor::GetMaxPlaintextByteSize(
     cricket::MediaType media_type,
     size_t encrypted_frame_size) {
  return encrypted_frame_size;
+}
+
+void gen_params(byte key[KEY_SIZE], byte iv[BLOCK_SIZE])
+{
+    int rc = RAND_bytes(key, KEY_SIZE);
+    if (rc != 1)
+       RTC_LOG(LS_VERBOSE) << "XXX RAND_bytes key failed";
+
+    rc = RAND_bytes(iv, BLOCK_SIZE);
+    if (rc != 1)
+      RTC_LOG(LS_VERBOSE) << "XXX RAND_bytes for iv failed";
+}
+
+void aes_encrypt(const byte key[KEY_SIZE], const byte iv[BLOCK_SIZE], const secure_string& ptext, secure_string& ctext)
+{
+    EVP_CIPHER_CTX_free_ptr ctx(EVP_CIPHER_CTX_new(), ::EVP_CIPHER_CTX_free);
+    int rc = EVP_EncryptInit_ex(ctx.get(), EVP_aes_256_cbc(), NULL, key, iv);
+    if (rc != 1)
+      RTC_LOG(LS_VERBOSE) << "XXX EVP_EncryptInit_ex failed";
+
+    // Recovered text expands upto BLOCK_SIZE
+    ctext.resize(ptext.size()+BLOCK_SIZE);
+    int out_len1 = (int)ctext.size();
+
+    rc = EVP_EncryptUpdate(ctx.get(), (byte*)&ctext[0], &out_len1, (const byte*)&ptext[0], (int)ptext.size());
+    if (rc != 1)
+      RTC_LOG(LS_VERBOSE) << "XXX EVP_EncryptUpdate failed";
+  
+    int out_len2 = (int)ctext.size() - out_len1;
+    rc = EVP_EncryptFinal_ex(ctx.get(), (byte*)&ctext[0]+out_len1, &out_len2);
+    if (rc != 1)
+      RTC_LOG(LS_VERBOSE) << "XXX EVP_EncryptFinal_ex failed";
+
+    // Set cipher text size now that we know it
+    ctext.resize(out_len1 + out_len2);
+}
+
+void aes_decrypt(const byte key[KEY_SIZE], const byte iv[BLOCK_SIZE], const secure_string& ctext, secure_string& rtext)
+{
+    EVP_CIPHER_CTX_free_ptr ctx(EVP_CIPHER_CTX_new(), ::EVP_CIPHER_CTX_free);
+    int rc = EVP_DecryptInit_ex(ctx.get(), EVP_aes_256_cbc(), NULL, key, iv);
+    if (rc != 1)
+      RTC_LOG(LS_VERBOSE) << "XXX EVP_DecryptInit_ex failed";
+
+    // Recovered text contracts upto BLOCK_SIZE
+    rtext.resize(ctext.size());
+    int out_len1 = (int)rtext.size();
+
+    rc = EVP_DecryptUpdate(ctx.get(), (byte*)&rtext[0], &out_len1, (const byte*)&ctext[0], (int)ctext.size());
+    if (rc != 1)
+      RTC_LOG(LS_VERBOSE) << "XXX EVP_DecryptUpdate failed";
+  
+    int out_len2 = (int)rtext.size() - out_len1;
+    rc = EVP_DecryptFinal_ex(ctx.get(), (byte*)&rtext[0]+out_len1, &out_len2);
+    if (rc != 1)
+      RTC_LOG(LS_VERBOSE) << "XXX EVP_DecryptFinal_ex failed";
+
+    // Set recovered text size now that we know it
+    rtext.resize(out_len1 + out_len2);
 }
 
 }
