d380f0e4a5d8c44a59178fea9373bdf2e88309bd
titus.moldovan <titus.moldovan@8x8.com>
2021-08-23 14:50:21 +0300

    to be squashed1

--- api/crypto/gcm_frame_decryptor.cc
+++ api/crypto/gcm_frame_decryptor.cc
@@ -58,7 +58,7 @@ int new_decrypt(unsigned char *ciphertext,
         RTC_LOG(LS_VERBOSE) << "XXX decryption final error------------------------" << myUniqueId << " " << media_type;
        // for (size_t i = 0 ; i < ciphertext_len; i++) {
        // RTC_LOG(LS_VERBOSE) << "XXX decryption initial------------------------" << myUniqueId<< " " << i << " " << ciphertext[i];
-    }
+      //}
 
         plaintext_len = -1;
      } else {
