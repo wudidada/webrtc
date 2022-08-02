71ff1156c2fb8ad3193d318b70129b84089dc1bd
Titus Moldovan <titus.moldovan@8x8.com>
2022-08-02 15:48:33 +0300

    fix2

--- sdk/android/src/jni/pc/gcm_frame_encryptor.cc
+++ sdk/android/src/jni/pc/gcm_frame_encryptor.cc
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+#include "rtc_base/logging.h"
 #include "sdk/android/generated_peerconnection_jni/GCMFrameEncryptor_jni.h"
 #include "sdk/android/src/jni/jni_helpers.h"
 
@@ -21,9 +22,12 @@ static void JNI_GCMFrameEncryptor_SetKey(
   jint* key_bytes_ptr = jni->GetIntArrayElements(key_bytes_array, 0);
   std::vector<uint8_t> key_bytes_vector(key_bytes_ptr, key_bytes_ptr + size);
 
+  RTC_LOG(LS_VERBOSE) << "XXX JNI_GCMFrameEncryptor_SetKey1";
   GCMFrameEncryptor* encryptor =
       reinterpret_cast<GCMFrameEncryptor*>(thiz.obj());
+  RTC_LOG(LS_VERBOSE) << "XXX JNI_GCMFrameEncryptor_SetKey2";
   encryptor->SetKey(key_bytes_vector);
+  RTC_LOG(LS_VERBOSE) << "XXX JNI_GCMFrameEncryptor_SetKey3";
 }
 
 }  // namespace jni
