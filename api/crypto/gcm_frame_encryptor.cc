de8907dd25f50d41bb3794fe05a737a73850eb2b
Titus Moldovan <titus.moldovan@8x8.com>
2022-08-03 10:38:10 +0300

    fix6

--- api/crypto/gcm_frame_encryptor.cc
+++ api/crypto/gcm_frame_encryptor.cc
@@ -9,6 +9,7 @@
 namespace webrtc {
 
 GCMFrameEncryptor::GCMFrameEncryptor() {
+  this->key_bytes.clear();
   this->key_bytes = { 97,  145, 133, 203, 63,  197, 49,  232, 87,  159, 169,
                      200, 59,  195, 77,  75,  150, 173, 189, 232, 44,  39,
                      8,   149, 250, 6,   238, 170, 255, 17,  110, 107 };
